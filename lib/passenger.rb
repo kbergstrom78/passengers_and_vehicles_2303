8c3e67c3e10e9fe6696f186c5c2f32057e923a20
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-04-05 09:14:43 -0600

    Initialize passenger

--- lib/passenger.rb
+++ lib/passenger.rb
@@ -4,9 +4,13 @@ class Passenger
               :age
 
   def initialize(passenger_info)
-    @name = passenger_info[:name]
-    @age = passenger_info[:age]
-
+    @name = passenger_info["name"]
+    @age = passenger_info["age"]
 
   end
+
+
+
+
+
 end
--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -3,16 +3,18 @@ require './lib/passenger'
 RSpec.describe Passenger do
 
   it 'is initialized' do
-    
     charlie = Passenger.new({"name" => "Charlie", "age" => 18})
     taylor = Passenger.new({"name" => "Taylor", "age" => 12})
 
     expect(charlie).to be_an_instance_of(Passenger)
   end
 
+  it 'has attributes' do
+    charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+    taylor = Passenger.new({"name" => "Taylor", "age" => 12})
 
-
-
-
+    expect(charlie.name).to eq("Charlie")
+    expect(charlie.age).to eq(18)
+  end
 
 end
