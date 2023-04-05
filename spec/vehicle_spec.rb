e92178b3dcf3677330da719adda6f09fcb0b13ec
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-04-05 09:32:48 -0600

    Add methods for speeding

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -8,4 +8,25 @@ RSpec.describe Vehicle do
 
     expect(vehicle).to be_an_instance_of(Vehicle)
   end
+
+  it 'has attributes' do
+    vehicle = Vehicle.new("2001", "Honda", "Civic")   
+
+    expect(vehicle.year).to eq("2001")
+    expect(vehicle.make).to eq("Honda")
+    expect(vehicle.model).to eq("Civic")
+  end
+
+  it 'can speed' do
+    vehicle = Vehicle.new("2001", "Honda", "Civic")  
+
+    expect(vehicle.speeding?).to eq(false)
+
+    vehicle.speed
+
+    expect(vehicle.speeding?).to eq(true)
+
+  end
+
+
 end
